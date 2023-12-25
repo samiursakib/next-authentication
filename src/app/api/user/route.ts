2863dfd423d1e7e4784fec586ee14169112aee2b
samiursakib <rafisamiur@gmail.com>
2023-12-26 03:38:03 +0600

    prisma setup completed

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,4 +1,19 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const getPostsOfFirstUser = async () => {
+  const firstUser = await prisma.user.findFirst();
+  const postsOfFirstUser = await prisma.post.findMany({
+    where: {
+      user: {
+        id: firstUser?.id
+      }
+    }
+  });
+  return postsOfFirstUser.slice(2);
+}
 
 export async function POST(req: Request) {
   const body = await req.json();
@@ -13,4 +28,15 @@ export async function POST(req: Request) {
     return NextResponse.json(user);
   }
   return NextResponse.json(null);
+}
+
+export async function GET(req: Request) {
+  try {
+    const result = await getPostsOfFirstUser();
+    return NextResponse.json(result);  
+  } catch(e) {
+    return NextResponse.json({ message: e });
+  } finally {
+    await prisma.$disconnect();
+  }
 }
