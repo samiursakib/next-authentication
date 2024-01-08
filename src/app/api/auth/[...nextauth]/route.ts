efe7df2176426162ef7b29d83a8e5bbe85d78288
samiursakib <rafisamiur@gmail.com>
2024-01-08 18:43:30 +0600

    avatar image fixed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,8 +5,3 @@ import { NextApiRequest, NextApiResponse } from "next";
 const handler = NextAuth(options);
 
 export { handler as GET, handler as POST }
-
-export async function auth(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader('Cache-Control', 'no-store, max-age=0');
-  return NextAuth(req, res, options);
-}
